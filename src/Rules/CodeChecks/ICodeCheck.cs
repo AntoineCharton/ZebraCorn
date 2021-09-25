31dbcaa7f11e68cb90ca941d41c6db54bddab2c5
Walter Hulsebos <walterhulsebos@outlook.com>
2021-09-25 02:20:37 +0200

    NEW: ICodeCheck
    Allows you to rate text for likelihood of being code on a [0-1] scale.

---
+++ src/Rules/CodeChecks/ICodeCheck.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ZebraCorn.Rules.CodeChecks
+{
+    internal interface ICodeCheck
+    {
+        /// <summary> Returns your the likelihood of this text being code on a [0-1] scale. </summary>
+        public Single CodeRating(String text);
+    }
+}
