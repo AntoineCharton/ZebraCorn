8ef3c97e2781de632f1e23944ef7b2b328b19af2
Walter Hulsebos <walterhulsebos@outlook.com>
2021-09-25 02:22:09 +0200

    NEW: (ICodeCheck) DefaultCodeCheck
    Maybe the naming is too verbose? Might refactor later.

---
+++ src/Rules/CodeChecks/DefaultCodeCheck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ZebraCorn.Rules.CodeChecks
+{
+    public class DefaultCodeCheck : ICodeCheck
+    {
+        public Single CodeRating(String text)
+        {
+            String[] code = {
+                "()", "void ", "class ", "public ", "private ", "internal ", "float ", "string ", "(this ", " = ", "-=", "+=", "++",
+            };
+                
+            Int32 count = text.CountOf(code);
+                
+            Console.WriteLine("DefaultCodeCheck Count = " + count);
+
+            return count;
+        }
+    }
+}
