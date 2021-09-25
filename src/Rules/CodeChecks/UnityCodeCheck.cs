07daf24260aa54f55794258c5ee9b65dcca23bd4
Walter Hulsebos <walterhulsebos@outlook.com>
2021-09-25 02:21:44 +0200

    NEW: (ICodeCheck) UnityCodeCheck

---
+++ src/Rules/CodeChecks/UnityCodeCheck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ZebraCorn.Rules.CodeChecks
+{
+    public class UnityCodeCheck : ICodeCheck
+    {
+        public Single CodeRating(String text)
+        {
+            String[] code = {
+                ": MonoBehaviour", "UnityEngine", "System.Collections", "System.Collections.Generic",
+                "void Start()", "void OnEnable()", "void OnDisable()", "void Update()", "void FixedUpdate()", 
+                "new Vector2", "new Vector3", "new Vector4", "new Quaternion", 
+            };
+
+            Int32 count = text.CountOf(code);
+                
+            Console.WriteLine("UnityCodeCheck Count = " + count);
+
+            return count;
+        }
+    }
+}
