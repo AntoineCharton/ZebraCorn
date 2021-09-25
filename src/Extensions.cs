a4da64f74d5eb0ea34b2c487777c95d74968cd8e
Walter Hulsebos <walterhulsebos@outlook.com>
2021-09-25 02:27:44 +0200

    NEW: Extensions - String.CountOf(String[]) & String.IndexesOf(String)

--- src/Extensions.cs
+++ src/Extensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using static System.Runtime.CompilerServices.MethodImplOptions;
@@ -11,5 +12,37 @@ namespace ZebraCorn
     {
         [MethodImpl(methodImplOptions: AggressiveInlining)]
         public static Boolean IsMod(this SocketUser author) => Program.Mods.Contains(author.Id.ToString()); //TODO: Actually Check Author Role?
+
+        public static Int32 CountOf(this String text, params String[] checks)
+        {
+            Int32 __count = 0;
+            
+            foreach (String __check in checks)
+            {
+                Int32 __index = 0;
+                
+                while ((__index = text.IndexOf(value: __check, startIndex: __index, comparisonType: StringComparison.Ordinal)) != -1)
+                {
+                    __index += __check.Length;
+                    __count++;
+                }
+            }
+            return __count;
+        }
+
+        public static Int32[] IndexesOf(this String text, String check)
+        {
+            Int32 __index = 0;
+
+            List<Int32> __occurrences = new();
+                
+            while ((__index = text.IndexOf(value: check, startIndex: __index, comparisonType: StringComparison.Ordinal)) != -1)
+            {
+                __occurrences.Add(__index);
+                __index += check.Length;
+            }
+
+            return __occurrences.ToArray();
+        }
     }
 }
