887e284661da25a972e4c22d25f9807f703cfe7b
Walter Hulsebos <walterhulsebos@outlook.com>
2021-09-25 02:21:07 +0200

    NEW: (ICodeCheck) SemicolonCheck

---
+++ src/Rules/CodeChecks/SemicolonCheck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace ZebraCorn.Rules.CodeChecks
+{
+    public class SemicolonCheck : ICodeCheck
+    {
+        public Single CodeRating(String text)
+        {
+            Int32[] __semicolonIndexes = text.IndexesOf(check: ";");
+                
+            Int32 endingsWithSemiColons = __semicolonIndexes.Select(index => text[index + 1] == '\n').Count();
+                
+            Console.WriteLine("Endings With Semicolons = " + endingsWithSemiColons);
+                
+            return endingsWithSemiColons;
+        }
+    }
+}
